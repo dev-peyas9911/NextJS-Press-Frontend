cd2dfe1f5372c0b1f3683b76b4757af8aa66b061
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2026-07-23 21:52:35 +0600

    User login successfully

--- app/(dashboardGroup)/layout.tsx
+++ app/(dashboardGroup)/layout.tsx
@@ -1,5 +1,14 @@
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+import { Navbar } from "@/components/shared/Navbar";
+import { getMe } from "@/service/getMe";
+
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+  const user = await getMe();
+  return (
+    <div>
+      <Navbar user={user}></Navbar>
+      {children}
+    </div>
+  );
 };
 
 export default DashboardLayout;
